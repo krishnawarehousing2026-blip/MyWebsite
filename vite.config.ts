35064dc271b70920b32e603e6d2f7e4b687b848d
krishnawarehousing2026-blip <krishnawarehousing2026@gmail.com>
2026-05-23 22:28:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
     react(),
-    tsconfigPaths(),
   ],
 })
